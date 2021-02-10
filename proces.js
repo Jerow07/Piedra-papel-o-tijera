71f221fc9fed2893d39d863ecedde68777f7ed73
Jeronimo <geronimoparra@hotmail.com>
2021-02-10 01:21:05 -0300

    FALTA SCORE PUNTAJE

--- proces.js
+++ proces.js
@@ -14,6 +14,33 @@ function cerrar(){
 
 
 
-function juego() {
+function juego(eleccion) {
+
+var elecciondecpu = Math.floor(Math.random()*(1 + 3))
+console.log (elecciondecpu)
+if (eleccion==elecciondecpu){
+    console.log("empate")
+} else if (1==eleccion){
+    if(elecciondecpu==2) {
+        console.log("You lose")
+    } else {
+        console.log("Win")
+    }
+}else if(2==eleccion){
+    if(elecciondecpu==1){
+        console.log("WIN")
+    }else {
+        console.log("You lose")
+    }
+
+}else if (3==eleccion){
+    if(elecciondecpu==1){
+        console.log("You lose")
+
+    }else {
+        console.log("Win")
+    }
+}
+
 
 }
